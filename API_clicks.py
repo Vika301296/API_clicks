4d24c6c243c9b02d10ba37313e485c93c28b3758
Vika301296 <106934640+Vika301296@users.noreply.github.com>
2023-08-03 02:22:55 +0300

    Add files via upload
    Add script file and requirements.txt

---
+++ API_clicks.py
@@ -0,0 +1,51 @@
+import requests
+
+from environs import Env
+
+
+def shorten_link(token, url):
+    headers = {'Authorization': f'Bearer {token}'}
+    payload = {
+        "long_url": f"{url}"
+    }
+    response = requests.post(url, json=payload, headers=headers)
+    response.raise_for_status()
+    bitlink = response.json()
+    return bitlink['id']
+
+
+def count_clicks(token, bitlink):
+    headers = {'Authorization': f'Bearer {token}'}
+    url = f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary'
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    return (response.json()['total_clicks'])
+
+
+def is_bitlink(passed_url):
+    headers = {'Authorization': f'Bearer {token}'}
+    url = f'https://api-ssl.bitly.com/v4/bitlinks/{passed_url}'
+    response = requests.get(url, headers=headers)
+    return response.ok
+
+
+if __name__ == "__main__":
+    env = Env()
+    env.read_env()
+    token = env.str('TOKEN')
+
+    try:
+        url = input('Введите ссылку: ')
+        if is_bitlink(url) is True:
+            total_clicks = count_clicks(token, url)
+            print(f'Total Clicks: {total_clicks}')
+        else:
+            long_url = url
+            url = 'https://api-ssl.bitly.com/v4/bitlinks'
+            bitlink = shorten_link(token, url)
+            print(f'Bitlink: {bitlink}')
+    except requests.exceptions.HTTPError as e:
+        if e.response.status_code == 400:
+            print("Bad Request Error 400:", e.response.json())
+        else:
+            print("Other Error:", e)
